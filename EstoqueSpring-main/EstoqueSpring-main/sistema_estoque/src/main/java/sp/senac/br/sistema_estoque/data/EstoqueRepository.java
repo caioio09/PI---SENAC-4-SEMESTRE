75b4623a3ce89d79841cf7decb8bc62327be468c
Gustavo Sanchez <127356014+GustavoSanchez-commit@users.noreply.github.com>
2024-11-06 23:16:11 -0300

    Delete EstoqueSpring-main directory

--- EstoqueSpring-main/EstoqueSpring-main/sistema_estoque/src/main/java/sp/senac/br/sistema_estoque/data/EstoqueRepository.java
+++
@@ -1,9 +0,0 @@
-package sp.senac.br.sistema_estoque.data;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import sp.senac.br.sistema_estoque.model.Estoque;
-
-public interface EstoqueRepository extends JpaRepository<Estoque, Long>{
-    
-}
