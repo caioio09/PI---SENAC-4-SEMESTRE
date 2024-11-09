a3ac6233018af4709bab16775bb1dbfb4e4c1961
Guilherme H V Santos <68071170+guilhermehvsantos@users.noreply.github.com>
2024-11-09 15:45:15 -0300

    Delete src directory

--- src/main/resources/static/data.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO categoria (nome) VALUES ('Alimentos');
-INSERT INTO categoria (nome) VALUES ('Bebidas');
-INSERT INTO categoria (nome) VALUES ('Brinquedos');
-INSERT INTO categoria (nome) VALUES ('Cosméticos');
-INSERT INTO categoria (nome) VALUES ('Hortifruti');
-INSERT INTO categoria (nome) VALUES ('Eletrônicos');
-INSERT INTO categoria (nome) VALUES ('Roupas');
