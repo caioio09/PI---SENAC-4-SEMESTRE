0e3d4d2957e421fa664bfff1073c06014953f5f5
Guilherme H V Santos <68071170+guilhermehvsantos@users.noreply.github.com>
2024-11-09 16:40:14 -0300

    Delete ADASPStock/src/main/resources/static/data.sql

--- ADASPStock/src/main/resources/static/data.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO categoria (id, nome) VALUES (1, 'Alimentos');
-INSERT INTO categoria (id, nome) VALUES (2, 'Bebidas');
-INSERT INTO categoria (id, nome) VALUES (3, 'Brinquedos');
-INSERT INTO categoria (id, nome) VALUES (4, 'Cosméticos');
-INSERT INTO categoria (id, nome) VALUES (5, 'Hortifruti');
-INSERT INTO categoria (id, nome) VALUES (6, 'Eletrônicos');
-INSERT INTO categoria (id, nome) VALUES (7, 'Roupas');
