ffeaed5dfcdfbec816931341cb8bdac0581ca447
Guilherme H V Santos <68071170+guilhermehvsantos@users.noreply.github.com>
2024-11-09 17:13:56 -0300

    Adicionando os scripts SQL

---
+++ ADASPStock/src/main/resources/data.sql
@@ -0,0 +1,12 @@
+-- data.sql
+-- Verificando se o script foi carregado
+SELECT 'Iniciando o script data.sql' AS Status;
+
+
+INSERT INTO categoria (nome) VALUES ('Alimentos');
+INSERT INTO categoria (nome) VALUES ('Bebidas');
+INSERT INTO categoria (nome) VALUES ('Brinquedos');
+INSERT INTO categoria (nome) VALUES ('Cosméticos');
+INSERT INTO categoria (nome) VALUES ('Hortifruti');
+INSERT INTO categoria (nome) VALUES ('Eletrônicos');
+INSERT INTO categoria (nome) VALUES ('Roupas');
